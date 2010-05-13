ef49c82f464f36db1ffac1954dce1a5c65e5aabf
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-05-13 11:52:10 -0300

    Ajustes Case Home, Busca, e Menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
     def index
       @Servidor =  "http://localhost:3002"
       @Highlights = Highlight.find(:all, :conditions => ['published = 1'])
+      
+      @Case = Case.find(:first, :conditions => ['published = 1'], :order => 'id DESC', :limit => 1)
     end
 end
