45d2faaa6a68190ee2e426357328ef2224079b5a
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-05-24 01:47:42 -0300

    Ajustes Produtos

--- app/controllers/linhas_controller.rb
+++ app/controllers/linhas_controller.rb
@@ -3,5 +3,21 @@ class LinhasController < ApplicationController
     @Section = Section.find(params[:id])
     
     @Categorias = @Section.categories.all(:conditions => ['published = 1']) 
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
+    
+    if @Section.id == 9
+      @HeaderInterno = "produtos/header_business.png" 
+    end
+    
+    if @Section.id == 10
+      @HeaderInterno = "produtos/header_solar.png" 
+    end
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
   end
 end
--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -7,5 +7,21 @@ class ProdutosController < ApplicationController
     @Produto = @Categoria.products.find(params[:id])
     
     @Imagens = @Produto.product_images.find(:all)
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
+    
+    if @Section.id == 9
+      @HeaderInterno = "produtos/header_business.png" 
+    end
+    
+    if @Section.id == 10
+      @HeaderInterno = "produtos/header_solar.png" 
+    end
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
   end
 end
