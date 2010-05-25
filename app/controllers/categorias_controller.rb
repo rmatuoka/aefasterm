4f4a4cadd5df7e0fb9c2936587e06e87b26e81c8
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-05-25 01:37:25 -0300

    Ajustes Produtos

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,10 +1,26 @@
 class CategoriasController < ApplicationController  
-  layout "blank"
+  #layout "blank"
   def show
     @Section = Section.find(params[:linha_id])
     
     @Categoria = @Section.categories.find(params[:id])
     
     @Produtos = @Categoria.products.find(:all, :conditions => ['published = 1'])
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
+    
+    if @Section.id == 9
+      @HeaderInterno = "produtos/header_business.png" 
+    end
+    
+    if @Section.id == 10
+      @HeaderInterno = "produtos/header_solar.png" 
+    end
+    
+    if @Section.id == 8
+      @HeaderInterno = "produtos/header_home.png" 
+    end
   end
 end
