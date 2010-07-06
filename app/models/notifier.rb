545ee9c81e92bf54234771496855c989c5f3000f
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-07-06 15:42:00 -0300

    Ajuste eail de contato

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,8 +1,8 @@
 class Notifier < ActionMailer::Base
     #se precisar os parametros são Email.deliver_padrao(corpo, "Assunto",email para&nbsp;&nbsp; #enviar(destino), email para resposta)
-    def enviar(corpo, assunto, email = "rmatuoka@agenciaeconomica.com.br",responder = "rmatuoka@agenciaeconomica.com.br")
+    def enviar(corpo, assunto, email = "site@fasterm.com.br",responder = "site@fasterm.com.br")
         recipients email
-        from "rmatuoka@agenciaeconomica.com.br"
+        from "site@fasterm.com.br"
         subject assunto
         reply_to responder
         body :corpo => corpo
